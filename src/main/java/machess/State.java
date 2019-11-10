1601f56da6a20a1036ca6f55a385fb35afe202d2
macsmol <mSmolczewski@gmail.com>
2019-11-10 16:34:13 +0100

    Corrected init, added printable symbols to pieces

--- src/main/java/machess/State.java
+++ src/main/java/machess/State.java
@@ -25,8 +25,11 @@ public class State {
 
 	/**
 	 * list of pieces. 7 bit per figure. 3 bit for file, 3 bits for row, 1 bit isAlive. Type of figure denoted by location.
-	 * fffrrrX
 	 * 7 bits * 32 figures = 224 bits -> 28 bytes
+	 *      b0      b1       b2       b3       b4       b5       b6
+	 * +--------+--------+--------+--------+--------+--------+--------+
+	 * |FFFRRRxF|FFRRRxFF|FRRRxFFF|RRRxFFFR|RRxFFFRR|RxFFFRRR|xFFFRRRx|
+	           7       14      21      28      35      42      49
 	 */
 	private byte[] pieces = new byte[28];
 
@@ -34,13 +37,19 @@ public class State {
 	   Initializes pieces for new game
 	*/
 	State() {
-		for (Piece piece : Piece.values()) {
-			byte pieceByte = readPieceAsByte(piece);
+		for (int i = 0; i < pieces.length; i++) {
+			// init first piece in this byte
+			int byteBeginInBits = i * Byte.SIZE;
+			Piece piece1 = Piece.findByBitIndex(byteBeginInBits);
+			byte pieceAsByte = piece1.initialValue;
+			pieces[i] = (byte)(pieceAsByte << (byteBeginInBits - piece1.bitIndex));
 
-			pieceByte |= piece.initialValue;
-
-			storePiece(piece, pieceByte);
-
+			// init first part of the other piece in this byte
+			int byteEndInBits = byteBeginInBits + Byte.SIZE - 1;
+			Piece piece2 = Piece.findByBitIndex(byteEndInBits);
+			byte piece2AsByte = piece2.initialValue;
+			// OR with prievious piece from this byte in order not to overwrite
+			pieces[i] = (byte)(pieces[i] | piece2AsByte >>> (piece2.bitIndex - byteBeginInBits));
 		}
 	}
 
@@ -94,45 +103,62 @@ public class State {
 	}
 
 	public enum Piece {
-		WHITE_PAWN_A(0, 2),
-		WHITE_PAWN_B(BITS_PER_PIECE, 2),
-		WHITE_PAWN_C(BITS_PER_PIECE * 2, 2),
-		WHITE_PAWN_D(BITS_PER_PIECE * 3, 2),
-		WHITE_PAWN_E(BITS_PER_PIECE * 4, 2),
-		WHITE_PAWN_F(BITS_PER_PIECE * 5, 2),
-		WHITE_PAWN_G(BITS_PER_PIECE * 6, 2),
-		WHITE_PAWN_H(BITS_PER_PIECE * 7, 2),
-		WHITE_KNIGHT_QS(BITS_PER_PIECE * 8, 2),
-		WHITE_KNIGHT_KS(BITS_PER_PIECE * 9, 2),
-		WHITE_BISHOP_QS(BITS_PER_PIECE * 10, 2),
-		WHITE_BISHOP_KS(BITS_PER_PIECE * 11, 2),
-		WHITE_ROOK_QS(BITS_PER_PIECE * 12, 2),
-		WHITE_ROOK_KS(BITS_PER_PIECE * 13, 2),
-		WHITE_QUEEN(BITS_PER_PIECE * 14, 2),
-		WHITE_KING(BITS_PER_PIECE * 15, 2),
-		BLACK_PAWN_A(BITS_PER_PIECE * 16, 2),
-		BLACK_PAWN_B(BITS_PER_PIECE * 17, 2),
-		BLACK_PAWN_C(BITS_PER_PIECE * 18, 2),
-		BLACK_PAWN_D(BITS_PER_PIECE * 19, 2),
-		BLACK_PAWN_E(BITS_PER_PIECE * 20, 2),
-		BLACK_PAWN_F(BITS_PER_PIECE * 21, 2),
-		BLACK_PAWN_G(BITS_PER_PIECE * 22, 2),
-		BLACK_PAWN_H(BITS_PER_PIECE * 23, 2),
-		BLACK_KNIGHT_QS(BITS_PER_PIECE * 24, 2),
-		BLACK_KNIGHT_KS(BITS_PER_PIECE * 25, 2),
-		BLACK_BISHOP_QS(BITS_PER_PIECE * 26, 2),
-		BLACK_BISHOP_KS(BITS_PER_PIECE * 27, 2),
-		BLACK_ROOK_QS(BITS_PER_PIECE * 28, 2),
-		BLACK_ROOK_KS(BITS_PER_PIECE * 29, 2),
-		BLACK_QUEEN(BITS_PER_PIECE * 30, 2),
-		BLACK_KING(BITS_PER_PIECE * 31, 2);
+		WHITE_PAWN_A(0, 0x06, '♙'),
+		WHITE_PAWN_B(BITS_PER_PIECE, 0x16, '♙'),
+		WHITE_PAWN_C(BITS_PER_PIECE * 2, 0x26, '♙'), // a2, alive
+		WHITE_PAWN_D(BITS_PER_PIECE * 3, 0x36, '♙'), // b2, alive
+		WHITE_PAWN_E(BITS_PER_PIECE * 4, 0x46, '♙'),
+		WHITE_PAWN_F(BITS_PER_PIECE * 5, 0x56, '♙'),
+		WHITE_PAWN_G(BITS_PER_PIECE * 6, 0x66, '♙'),
+		WHITE_PAWN_H(BITS_PER_PIECE * 7, 0x76, '♙'),
+		WHITE_KNIGHT_QS(BITS_PER_PIECE * 8, 0x22,'♘'), // b1, alive
+		WHITE_KNIGHT_KS(BITS_PER_PIECE * 9, 0xC2,'♘'), // g1, alive
+		WHITE_BISHOP_QS(BITS_PER_PIECE * 10, 0x32,'♗'), // c1, alive
+		WHITE_BISHOP_KS(BITS_PER_PIECE * 11, 0xB2,'♗'), // f1, alive
+		WHITE_ROOK_QS(BITS_PER_PIECE * 12, 0x12,'♖'), // a1, alive
+		WHITE_ROOK_KS(BITS_PER_PIECE * 13, 0xC2,'♖'), // h1, alive
+		WHITE_QUEEN(BITS_PER_PIECE * 14, 0x62,'♕'), // d1, alive
+		WHITE_KING(BITS_PER_PIECE * 15, 0x72,'♔'), // e1, alive
+		BLACK_PAWN_A(BITS_PER_PIECE * 16, 0x1A, '♟'), // a7, alive
+		BLACK_PAWN_B(BITS_PER_PIECE * 17, 0x3A, '♟'), // b7, alive
+		BLACK_PAWN_C(BITS_PER_PIECE * 18, 0x5A, '♟'),
+		BLACK_PAWN_D(BITS_PER_PIECE * 19, 0x7A, '♟'),
+		BLACK_PAWN_E(BITS_PER_PIECE * 20, 0x9A, '♟'),
+		BLACK_PAWN_F(BITS_PER_PIECE * 21, 0xBA, '♟'),
+		BLACK_PAWN_G(BITS_PER_PIECE * 22, 0xDA, '♟'),
+		BLACK_PAWN_H(BITS_PER_PIECE * 23, 0xFA, '♟'),
+		BLACK_KNIGHT_QS(BITS_PER_PIECE * 24, 0x3E, '♞'), // b8, alive
+		BLACK_KNIGHT_KS(BITS_PER_PIECE * 25, 0xDE,'♞'), // g8, alive
+		BLACK_BISHOP_QS(BITS_PER_PIECE * 26, 0x5E, '♝'), // c8, alive
+		BLACK_BISHOP_KS(BITS_PER_PIECE * 27, 0xAE,'♝'), // f8, alive
+		BLACK_ROOK_QS(BITS_PER_PIECE * 28, 0x1E, '♜'), // a8, alive
+		BLACK_ROOK_KS(BITS_PER_PIECE * 29, 0xFE, '♜'), // h8, alive
+		BLACK_QUEEN(BITS_PER_PIECE * 30, 0x7E,'♛'), // d8, alive
+		BLACK_KING(BITS_PER_PIECE * 31, 0x9E,'♚'); // e8, alive
 
+		/**
+		 * Index to pieces array, in bits.
+		 */
 		public final int bitIndex;
-		public final int initialValue;
+		
+		/**
+		 Initial position and alive flag, lowest bit unset (has no meaning)
+		 */
+		public final byte initialValue;
 
-		Piece(int bitIndex, int initialValue) {
+		/**
+		 * printable symbol
+		 */
+		public final char symbol;
+
+		Piece(int bitIndex, int initialValue, char symbol) {
 			this.bitIndex = bitIndex;
-			this.initialValue = initialValue;
+			this.initialValue = (byte)initialValue;
+			this.symbol = symbol;
+		}
+
+		static Piece findByBitIndex(int bitIndex) {
+			return Piece.values()[bitIndex / BITS_PER_PIECE];
 		}
 	}
 }
