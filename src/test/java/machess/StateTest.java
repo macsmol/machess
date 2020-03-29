19eea8df4f1a00a69ec444628514f2d61522ed57
macsmol <mSmolczewski@gmail.com>
2020-03-29 15:21:45 +0200

    improve tests

--- src/test/java/machess/StateTest.java
+++ src/test/java/machess/StateTest.java
@@ -9,8 +9,8 @@ public class StateTest {
 
     @Test
     public void moveGenerationTest() {
-        testPosition("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", new long[] {1, 48, 2039, 97862, 4085603, 193690690});
-        testPosition("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1", new long[] {1 ,24, 496, 9483, 182838, 3605103});
+        testPosition("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", new long[] {48, 2039, 97862,   4085603, 193690690});
+        testPosition("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1",                              new long[] {24, 496, 9483,     182838, 3605103});
     }
 
     private void testPosition(String fen, int depth, long expectedLegalMoves) {
@@ -26,10 +26,11 @@ public class StateTest {
     private void testPosition(String fen, long[] expectedLegalMoves) {
         State position = FEN.parse(fen);
         System.out.println("Testing: " + fen);
-        for (int depth = 0; depth < expectedLegalMoves.length; depth++) {
+        for (int i = 0; i < expectedLegalMoves.length; i++) {
+            int depth = i + 1;
             long actualLegalMoves = Scorer.perft(position, depth);
-            if (expectedLegalMoves[depth] != actualLegalMoves) {
-                System.out.println("Wrong perft(" + depth + "). Expected: " + expectedLegalMoves[depth] + ", actual: "+ actualLegalMoves +". Position: " + position);
+            if (expectedLegalMoves[i] != actualLegalMoves) {
+                System.out.println("Wrong perft(" + depth + "). Expected: " + expectedLegalMoves[i] + ", actual: "+ actualLegalMoves +". Position: " + position);
                 Scorer.perftDivide(position, depth);
                 fail();
             }
