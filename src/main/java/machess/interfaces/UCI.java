6138ec49fc430a8d569b131889b465c8c9eeeb63
macsmol <mSmolczewski@gmail.com>
2020-11-29 17:27:49 +0100

    move ordering - pawn takes go first

--- src/main/java/machess/interfaces/UCI.java
+++ src/main/java/machess/interfaces/UCI.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import static machess.Utils.*;
 
 public class UCI {
-    private static final String VERSION_STRING = "1.0-SNAPSHOT_25.11.2020";
+    private static final String VERSION_STRING = "1.0-SNAPSHOT-A-B-pawnTakes_29.11.2020";
     public static final String POSITION = "position";
     public static final String STARTPOS = "startpos";
     public static final String MOVES = "moves";
