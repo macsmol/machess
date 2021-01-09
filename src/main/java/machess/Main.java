0ab7cfc5d35d0b6c60f25400290cde345aaef2e4
macsmol <mSmolczewski@gmail.com>
2021-01-09 16:37:07 +0100

    shorten Main

--- src/main/java/machess/Main.java
+++ src/main/java/machess/Main.java
@@ -4,10 +4,6 @@ import machess.interfaces.UCI;
 
 public class Main {
     public static void main(String[] args) {
-        startUci();
-    }
-
-    private static void startUci() {
         UCI uci = new UCI();
         uci.startEngine();
     }
